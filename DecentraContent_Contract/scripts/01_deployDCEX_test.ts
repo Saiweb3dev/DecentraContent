b3ad0e98bf951380f46a88bf83607efcadc40f0c
saik434455@gmail.com <saik434455@gmail.com>
2024-04-27 19:03:11 +0530

    deployment script for DCEX_test

---
+++ DecentraContent_Contract/scripts/01_deployDCEX_test.ts
@@ -0,0 +1,23 @@
+import hre from "hardhat";
+//npx hardhat run scripts/01_deployDCEX_test.ts
+const main = async () => {
+  const DCEX_Factory =
+    await hre.ethers.getContractFactory("DCEXwithEscrowTest");
+  const DCEX = await DCEX_Factory.deploy();
+  const contractAddress: string = DCEX.target.toString();
+
+  const DCEX_interface = DCEX.interface.formatJson();
+  const abi = JSON.parse(DCEX_interface);
+  console.log("Contract Address -> ", contractAddress);
+  console.log("Contract Abi -> ", abi);
+  await hre.deployments.save("DCEXwithEscrowTest", {
+    abi: abi,
+    address: contractAddress,
+  });
+};
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
