e50cce448976809f31b7835030c2a2a06d17c617
saik434455@gmail.com <saik434455@gmail.com>
2024-04-29 11:02:33 +0530

    tx receipt for contract deployment and gas

--- DecentraContent_Contract/scripts/01_deployDCEX_test.ts
+++ DecentraContent_Contract/scripts/01_deployDCEX_test.ts
@@ -4,12 +4,16 @@ const main = async () => {
   const DCEX_Factory =
     await hre.ethers.getContractFactory("DCEXwithEscrowTest");
   const DCEX = await DCEX_Factory.deploy();
+
+  const tx = await DCEX.deploymentTransaction()?.wait()
+  // console.log("Transaction -> ",tx)
+  console.log("Gas used:", tx?.gasUsed.toString());
   const contractAddress: string = DCEX.target.toString();
 
   const DCEX_interface = DCEX.interface.formatJson();
   const abi = JSON.parse(DCEX_interface);
-  console.log("Contract Address -> ", contractAddress);
-  console.log("Contract Abi -> ", abi);
+  // console.log("Contract Address -> ", contractAddress);
+  // console.log("Contract Abi -> ", abi);
   await hre.deployments.save("DCEXwithEscrowTest", {
     abi: abi,
     address: contractAddress,
