2fc24a7806c744b0fba5a7efda65a236899e9970
saik434455@gmail.com <saik434455@gmail.com>
2024-05-25 10:13:05 +0530

    depreciated code updated

--- DecentraContent_NextJS/src/app/api/files/route.ts
+++ DecentraContent_NextJS/src/app/api/files/route.ts
@@ -1,19 +1,12 @@
 import { NextResponse, NextRequest } from "next/server";
 
-export const config = {
- api: {
-    bodyParser: false,
- },
-};
-
 export async function POST(request: NextRequest) {
- try {
+  try {
     const data = await request.formData();
     const file: File | null = data.get("file") as unknown as File;
-    const metadata = data.get("metadata"); // Assuming metadata is sent as a string
-    const metadataString = metadata !== null ? String(metadata) : null;
+    const metadata = data.get("metadata");
+    const metadataString = metadata!== null? String(metadata) : null;
 
-    // Step 1: Upload the file to Pinata to get the IPFS hash
     const formData = new FormData();
     formData.append("file", file);
 
@@ -26,13 +19,10 @@ export async function POST(request: NextRequest) {
     });
 
     const { IpfsHash: fileIpfsHash } = await res.json();
-    console.log("File IPFS Hash:", fileIpfsHash);
 
-    // Step 2: Add the file's IPFS hash to the metadata as a URL
-    const parsedMetadata = metadataString ? JSON.parse(metadataString) : {};
-    parsedMetadata.url = `ipfs://${fileIpfsHash}`; // Assuming you want to add the URL in this format
+    const parsedMetadata = metadataString? JSON.parse(metadataString) : {};
+    parsedMetadata.url = `ipfs://${fileIpfsHash}`;
 
-    // Step 3: Upload the updated metadata to Pinata to get the IPFS hash for the JSON
     const metadataFormData = new FormData();
     metadataFormData.append("file", new Blob([JSON.stringify(parsedMetadata)], { type: "application/json" }));
 
@@ -45,18 +35,12 @@ export async function POST(request: NextRequest) {
     });
 
     const { IpfsHash: metadataIpfsHash } = await metadataRes.json();
-    console.log("Metadata IPFS Hash:", metadataIpfsHash);
 
-    // Construct the gateway URL for the JSON metadata
     const metadataGatewayUrl = `https://gateway.pinata.cloud/ipfs/${metadataIpfsHash}`;
 
-    // Return both the file IPFS hash and the metadata IPFS hash
     return NextResponse.json({ fileIpfsHash, metadataIpfsHash, metadataGatewayUrl }, { status: 200 });
- } catch (e) {
-    console.log(e);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
- }
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
